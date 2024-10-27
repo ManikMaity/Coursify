b36ed78f9d9cec16661321d3467f6f8ac616e55d
Manik Maity <manikmaity156@gmail.com>
2024-10-27 09:09:16 +0530

    Changed back to diployed api

--- frontend/src/helper/axiosInstance.js
+++ frontend/src/helper/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-    // baseURL : "https://coursify-assignment-api.onrender.com"
-    baseURL : "http://localhost:3000"
+    baseURL : "https://coursify-assignment-api.onrender.com"
+    // baseURL : "http://localhost:3000"
 })
 
 export default axiosInstance;
